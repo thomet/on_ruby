77ade31312c13805adc0b0da5e914806ab505894
Peter Schröder <phoetmail@googlemail.com>
2013-04-04 21:23:31 +0200

    use ruby 2.0.0

--- config/deploy/rvm.rb
+++ config/deploy/rvm.rb
@@ -3,7 +3,7 @@ require 'rvm/capistrano'
 set :rvm_type, :user
 set :rvm_install_type, :stable
 set :rvm_install_ruby, :install
-set :rvm_ruby_string, 'ruby-1.9.3-p385@on_ruby'
+set :rvm_ruby_string, 'ruby-2.0.0-p0@on_ruby'
 
 before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
